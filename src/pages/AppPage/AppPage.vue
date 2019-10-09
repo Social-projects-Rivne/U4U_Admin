7512f326fbda7cc4593b3d2076d12af424ab251d
Andrii Druzhytskii <d.andriiy@gmail.com>
2019-10-09 15:08:38 +0300

    Add admin logout

--- src/pages/AppPage/AppPage.vue
+++ src/pages/AppPage/AppPage.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
         <h1>App Page</h1>
+        <logout-button/>
     </div>
 </template>
+
+<script>
+import LogoutButton from '../../components/controls/buttons/LogoutButton/LogoutButton.vue'
+
+export default {
+    components: {
+        LogoutButton
+    }
+}
+</script>
