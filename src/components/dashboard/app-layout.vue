be5e995ec3f94f5ec96c19902b215bdfdbb099f9
Dima Basiuk <dimabasyk993@gmail.com>
2019-10-28 11:19:26 +0200

    edit main table

--- src/components/dashboard/app-layout.vue
+++ src/components/dashboard/app-layout.vue
@@ -2,25 +2,30 @@
     <div class="grid">
         <app-header></app-header>
         <app-sidebar></app-sidebar>
-        <app-table></app-table>
+        <div class="content">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
 <script>
+
 import AppHeader from "@/components/dashboard/app-header.vue";
 import AppSidebar from "@/components/dashboard/app-sidebar.vue";
-import AppTable from "@/components/dashboard/table/table-layout.vue";
 
 export default {
     name: "app-layout",
     components: {
         AppHeader,
         AppSidebar,
-        AppTable
     }
 };
 </script>
 
 <style lang="scss" scoped>
 @import "dashboard.scss";
+
+    .content {
+        padding-top: 1rem;
+    }
 </style>
---
+++ src/pages/app-baned-users.vue
@@ -0,0 +1,172 @@
+<template>
+  <app-layout>
+   <app-table :cols="cols" :rows="rows"></app-table>
+  </app-layout>
+</template>
+
+
+
+<script>
+    import AppLayout from "@/components/dashboard/app-layout";
+    import AppTable from "@/components/table/table-layout.vue";
+
+
+    export default {
+        name: "app-baned-users",
+        data() {
+          return {
+              cols: [
+                  {
+                      id: 'counter',
+                      label: '#',
+                  },
+                  {
+                      id: 'nickName',
+                      label: 'Nick name',
+                  },
+                  {
+                      id: 'bannedBy',
+                      label: 'Banned by',
+                  },
+                  {
+                      id: 'dateFrom',
+                      label: 'From',
+                  },
+                  {
+                      id: 'dateTo',
+                      label: 'To',
+                  },
+                  {
+                      id: 'reason',
+                      label: 'Reason',
+                  }
+              ],
+              rows: []
+          }
+        },
+        methods: {
+          createData(data) {
+           //const { nickName, bunnedBy, to reason } = data;
+              return [
+                  {
+                      id: 'customId',
+                      counter: '1',
+                      nickName: 'Jerry',
+                      bannedBy: 'Tom Jenkins',
+                      dateFrom: '2019-01-01',
+                      dateTo: '2019-01-02',
+                      reason: 'some reason'
+                  }
+              ]
+          },
+        },
+        created() {
+            this.rows = [
+                {
+                    id: 'customId',
+                    counter: '1',
+                    nickName: 'Jerry',
+                    bannedBy: 'Tom Jenkins',
+                    dateFrom: '2019-01-01',
+                    dateTo: '2019-01-02',
+                    reason: 'some reason'
+                },
+                 {
+                    id: 'customId1',
+                    counter: '2',
+                    nickName: 'Jerry',
+                    bannedBy: 'Tom Jenkins',
+                    dateFrom: '2019-01-01',
+                    dateTo: '2019-01-02',
+                    reason: 'some reason'
+                },
+                 {
+                    id: 'customId2',
+                    counter: '3',
+                    nickName: 'Jerry',
+                    bannedBy: 'Tom Jenkins',
+                    dateFrom: '2019-01-01',
+                    dateTo: '2019-01-02',
+                    reason: 'some reason'
+                }, {
+                    id: 'customId3',
+                    counter: '4',
+                    nickName: 'Jerry',
+                    bannedBy: 'Tom Jenkins',
+                    dateFrom: '2019-01-01',
+                    dateTo: '2019-01-02',
+                    reason: 'some reason'
+                },
+                 {
+                    id: 'customId4',
+                    counter: '5',
+                    nickName: 'Jerry',
+                    bannedBy: 'Tom Jenkins',
+                    dateFrom: '2019-01-01',
+                    dateTo: '2019-01-02',
+                    reason: 'some reason'
+                },
+                 {
+                    id: 'customId5',
+                    counter: '6',
+                    nickName: 'Jerry',
+                    bannedBy: 'Tom Jenkins',
+                    dateFrom: '2019-01-01',
+                    dateTo: '2019-01-02',
+                    reason: 'some reason'
+                },
+                 {
+                    id: 'customId6',
+                    counter: '7',
+                    nickName: 'Jerry',
+                    bannedBy: 'Tom Jenkins',
+                    dateFrom: '2019-01-01',
+                    dateTo: '2019-01-02',
+                    reason: 'some reason'
+                },
+                 {
+                    id: 'customId7',
+                    counter: '8',
+                    nickName: 'Jerry',
+                    bannedBy: 'Tom Jenkins',
+                    dateFrom: '2019-01-01',
+                    dateTo: '2019-01-02',
+                    reason: 'some reason'
+                },
+                 {
+                    id: 'customId8',
+                    counter: '1',
+                    nickName: 'Jerry',
+                    bannedBy: 'Tom Jenkins',
+                    dateFrom: '2019-01-01',
+                    dateTo: '2019-01-02',
+                    reason: 'some reason'
+                }, {
+                    id: 'customId9',
+                    counter: '1',
+                    nickName: 'Jerry2',
+                    bannedBy: 'Tom Jenkins',
+                    dateFrom: '2019-01-01',
+                    dateTo: '2019-01-02',
+                    reason: 'some reason'
+                }, {
+                    id: 'customId10',
+                    counter: '1',
+                    nickName: 'Jerry',
+                    bannedBy: 'Tom Jenkins',
+                    dateFrom: '2019-01-01',
+                    dateTo: '2019-01-02',
+                    reason: 'some reason'
+                }
+            ]
+        },
+        components: {
+            AppLayout,
+            AppTable
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
