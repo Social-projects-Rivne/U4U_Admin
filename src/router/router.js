2b5ae60fa1f2a0d486806aa83c4cf8b5cbb06824
Dmitry Chekurkov <chekurkov@gmail.com>
2019-10-31 14:32:30 +0200

    In this commit i protected moderators route from moderators

--- src/router/router.js
+++ src/router/router.js
@@ -69,7 +69,7 @@ const router = new VueRouter({
             name: 'moderators',
             component: Moderators,
             meta: {
-                requiresAuth: true
+                isAdmin: true
             }
         },
         { 
