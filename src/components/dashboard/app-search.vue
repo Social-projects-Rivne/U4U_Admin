40497ea15bca35b4c93bce2bde5a3568b49a01cb
Viktor <work.morpi@gmail.com>
2019-11-16 16:20:22 +0200

    added search component

---
+++ src/components/dashboard/app-search.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header-search">
+    <input
+      type="text"
+      class="header-input"
+      placeholder="Search..."
+      v-model="search"
+      @keyup.enter="submit"
+      id="search"
+    />
+    <button for="search" class="header-button" @click="submit">
+      <font-awesome-icon icon="search" />search
+    </button>
+    <button class="header-button" @click="clear">clear</button>
+  </div>
+</template>
+
+<script>
+import { EventBus } from "../../../event-bus.js";
+export default {
+  name: "app-search",
+  data() {
+    return {
+      search: ""
+    };
+  },
+  methods: {
+    submit() {
+      EventBus.$emit("inputData", this.search);
+    },
+    clear() {
+      this.search = "";
+      EventBus.$emit("inputData", this.search);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "dashboard.scss";
+</style>
