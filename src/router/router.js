65d78589667399f75c51ec7180df7e74e13391b2
andrii-kondratiuk <kondratyuk.andy@gmail.com>
2019-12-08 02:32:43 +0200

    login redirects to dashboard, app page cleared

--- src/router/router.js
+++ src/router/router.js
@@ -41,8 +41,8 @@ const router = new VueRouter({
 
             meta: {
                 guest: false,
-                admin: true,
-                moderator: true
+                admin: false,
+                moderator: false
             }
         },
         {
